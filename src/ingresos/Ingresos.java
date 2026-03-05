7cb7c8b048f49cc0f84eb31e930027f40293f8a5
franksanchez205 <franksanchez205@gmail.com>
2026-03-05 18:39:48 -0500

    Se agrego la clase ingresos

---
+++ src/ingresos/Ingresos.java
@@ -0,0 +1,17 @@
+package ingresos;
+
+public class Ingresos {
+    private String fuente;
+    private double monto;
+    public Ingresos(String fuente, double monto) {
+        this.fuente = fuente;
+        this.monto = monto;
+    }
+    public String getFuente() {
+        return fuente;
+    }
+    public double getMonto() {
+        return monto;
+    }
+    
+}
